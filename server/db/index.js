f0b7099c0213f1bd2b31080cf005f698bcd517ba
dwangdi1 <75458457+dwangdi1@users.noreply.github.com>
2023-07-07 06:50:17 -0700

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -56,7 +56,7 @@ const fetchMovieDetails = async (movieId) => {
 const syncAndSeed = async () => {
   await conn.sync({ force: false });
 
-  const totalPages = 100;
+  const totalPages = 20;
 
   try {
 
