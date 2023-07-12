0d71d81406e3908827d03fe35b75b404bccad303
ellie <flowerskullxoxo@gmail.com>
2023-07-12 13:17:14 -0700

    responsive search bars

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -123,7 +123,7 @@ const Home = () => {
          </div>
          
           <button
-            className=" inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
             onClick={findLink}
           >
             Find Link
