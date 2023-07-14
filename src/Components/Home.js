cdb85d8545c875d2b8778b207c291ed054c529f1
dwangdi1 <75458457+dwangdi1@users.noreply.github.com>
2023-07-14 09:19:24 -0700

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -178,6 +178,7 @@ const Home = () => {
         </div>
           <button
             className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            disabled={casts1Id.trim() === '' || casts2Id.trim() === ''}
             onClick={findLink}
           >
             Find Link
