f7c3685ed9722bddbe99178ac2b1213ff7b34eb0
chee86j <jeffreychee86@gmail.com>
2023-09-01 13:04:11 -0400

    updated footer linkedin images

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,28 +17,28 @@ const Footer = () => {
         <a href="https://www.linkedin.com/in/dorjeewangdi/">
           <img
             className="h-10 w-10 rounded-lg opacity-70 hover:opacity-100 hover:scale-110"
-            src="https://media.licdn.com/dms/image/D5603AQHBa_04FHXJhQ/profile-displayphoto-shrink_800_800/0/1685257545695?e=1693440000&v=beta&t=2Fslbq3xzwHYk_EKXGWiMUxNibN_-CLZJ2jWCADKCcs"
+            src="https://media.licdn.com/dms/image/D5635AQEh7kL96IDyew/profile-framedphoto-shrink_800_800/0/1691084633767?e=1694120400&v=beta&t=0fiZObB4hajGeSIDx5Gbbgg1VHDgpMQLqENDQ0X_XzU"
             alt="Dorjee Wangdi"
           />
         </a>
         <a href="https://www.linkedin.com/in/ellie-tetelboym/">
           <img
             className="h-10 w-10 rounded-lg opacity-70 hover:opacity-100 hover:scale-110"
-            src="https://media.licdn.com/dms/image/D4D03AQGTGJGU2EixBg/profile-displayphoto-shrink_800_800/0/1685512899795?e=1693440000&v=beta&t=3OpB8pmoZO0nba25NklQ5kTnCKrALUOIlHWE7DQtv_M"
+            src="https://media.licdn.com/dms/image/D4D03AQGTGJGU2EixBg/profile-displayphoto-shrink_800_800/0/1685512899795?e=1698883200&v=beta&t=_V2CdzV1KiJWGnJhsBiNXXEulubpe2qD5dO_y0BfzoM"
             alt="Ellie Tetelboym"
           />
         </a>
         <a href="https://www.linkedin.com/in/jeffchee86/">
           <img
             className="h-10 w-10 rounded-lg opacity-70 hover:opacity-100 hover:scale-110"
-            src="https://media.licdn.com/dms/image/D4E03AQFHhmuzadJ-IA/profile-displayphoto-shrink_800_800/0/1685372958148?e=1693440000&v=beta&t=0DZjSndAxBBuDIMnknNM1wtD3ZSjI1p__uKl_-wPlUg"
+            src="https://media.licdn.com/dms/image/D4E03AQFHhmuzadJ-IA/profile-displayphoto-shrink_800_800/0/1685372958148?e=2147483647&v=beta&t=wSqHGonDBrdK8IEjIp93QI6YETmZLZnm777WJDojiAE"
             alt="Jeffrey Chee"
           />
         </a>
         <a href="https://www.linkedin.com/in/grant-way/">
           <img
             className="h-10 w-10 rounded-lg opacity-70 hover:opacity-100 hover:scale-110"
-            src="https://media.licdn.com/dms/image/D5603AQE_VM4dCnYWOg/profile-displayphoto-shrink_800_800/0/1685539963458?e=1693440000&v=beta&t=m4cP1mztlKoA7jklxQ4xEP6IjaFhqK9cfMzKw7sUZOk"
+            src="https://media.licdn.com/dms/image/D5635AQEkGQEe7UI4Jg/profile-framedphoto-shrink_800_800/0/1690220836570?e=1694120400&v=beta&t=B_ggqffDo6JN8h7QyNO2-zFAAEzVNLb3RRF09kwapL0"
             alt="Grant Way"
           />
         </a>
