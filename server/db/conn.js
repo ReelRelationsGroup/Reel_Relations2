8b0d99d0931b59fada00dfab7180ab34af211769
chee86j <105406369+chee86j@users.noreply.github.com>
2023-09-19 00:18:23 -0400

    Update conn.js
     Modified the Sequelize connection configuration to use SSL to build on render.com

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,6 +4,15 @@ const config = {};
 if (process.env.QUIET) {
   config.logging = false;
 }
+
+// Add SSL configuration (for render.com)
+config.dialectOptions = {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+};
+
 const conn = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost/reel_relations_db",
   config
